10091e51e7beb61826650b8d629b03d4c3fe1cfc
frenkiart <frenki_art@mail.ru>
2021-05-13 12:32:22 +0300

    feat: Сделал работающий функционал app.post по созданию новых пользователей. записываю их в отдеьный файлик.

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,14 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+const fs = require('fs');
 
+const filePath = 'src/resources/data/user.json';
+
+const getAll = async () =>
+  // TODO: mock implementation. should be replaced during task development
+  // [];
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) throw err;
+
+    return JSON.parse(data);
+  });
 module.exports = { getAll };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,53 @@
 const router = require('express').Router();
+const fs = require('fs');
 const User = require('./user.model');
 const usersService = require('./user.service');
 
+const filePath = 'src/resources/data/user.json';
+
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
+
   // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  setTimeout(() => {
+    res.status(200).json(users.map(User.toResponse));
+  }, 3000);
 });
+
+/* router.route('/:userId').get(async (req, res) => {
+  console.log(req, res);
+}); */
+
+router.route('/').post(async (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+
+  const userArgs = req.body;
+  const newUser = new User(userArgs);
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    const users = JSON.parse(data);
+    users.push(newUser);
+
+    const newData = JSON.stringify(users);
+
+    fs.writeFile(filePath, newData, (error) => {
+      if (error) throw err;
+    });
+  });
+
+  res.sendStatus(201);
+});
+
+/* router.route('/:userId').put(async (req, res) => {
+  console.log(req, res);
+});
+
+router.route('/:userId').delete(async (req, res) => {
+  console.log(req, res);
+}); */
 
 module.exports = router;
