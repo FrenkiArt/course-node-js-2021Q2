5ef8f8e5e195fcd4f8e7d933f28d36433d0f7fa1
frenkiart <frenki_art@mail.ru>
2021-06-17 22:59:12 +0300

    feat: первые более менее успешные коннекшены к базе данных

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,26 @@ import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import config from './common/config';
 import app from './app';
+// import User from './entity/user.model';
 
 // create connection with database
 // note that it's not active database connection
 // TypeORM creates connection pools and uses them for your requests
 createConnection()
   .then(async (connection) => {
+    // (connection)
+    console.log('connection is:', connection);
+
     app.listen(config.PORT, () => {
       console.log(`App is running on http://localhost:${config.PORT}`);
     });
+
+    /*  await connection.manager.save(
+      connection.manager.create(User, {
+        name: 'Arthur',
+        login: 'Best',
+        password: 'password',
+      })
+    ); */
   })
   .catch((error) => console.log('TypeORM connection error: ', error));
